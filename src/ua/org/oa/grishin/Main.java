e3eef95d216e9effd8265cc2853612ea20402aa0
Yuri Grishin <y.y.grishin@gmail.com>
2015-02-02 19:26:19 +0200

    Add project files

---
+++ src/ua/org/oa/grishin/Main.java
@@ -0,0 +1,20 @@
+package ua.org.oa.grishin;
+/**
+ * 
+ */
+
+/**
+ * @author Yuri Grishin
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
